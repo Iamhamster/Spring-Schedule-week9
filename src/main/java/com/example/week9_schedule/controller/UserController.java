d0944973508872f785b9294cb7ea975d00b51630
Iamhamster <gksdbsgml313@naver.com>
2025-02-10 15:19:48 +0900

    유저 컨트롤러 추가

--- src/main/java/com/example/week9_schedule/controller/UserController.java
+++ src/main/java/com/example/week9_schedule/controller/UserController.java
@@ -1,4 +1,44 @@
 package com.example.week9_schedule.controller;
 
+import com.example.week9_schedule.dto.ScheduleRequestDto;
+import com.example.week9_schedule.dto.ScheduleResponseDto;
+import com.example.week9_schedule.dto.UserRequestDto;
+import com.example.week9_schedule.dto.UserResponseDto;
+import com.example.week9_schedule.service.ScheduleService;
+import com.example.week9_schedule.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<UserResponseDto> save(@RequestBody UserRequestDto dto){
+        return ResponseEntity.ok(userService.save(dto));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponseDto>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserResponseDto> findOne(@PathVariable Long id){
+        return ResponseEntity.ok(userService.findOne(id));
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<UserResponseDto> update(@PathVariable Long id, @RequestBody UserRequestDto dto){
+        return ResponseEntity.ok(userService.update(id, dto));
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable Long id){
+        userService.delete(id);
+    }
 }
